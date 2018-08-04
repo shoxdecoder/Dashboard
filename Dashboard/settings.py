4d91793f53ce300632c60faaf483f5a4bdf59540
shoxdecoder <lukasz@pawlicki.info>
2018-08-04 18:00:42 +0200

    Tutorial part 2

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ReportOne.apps.ReportoneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
